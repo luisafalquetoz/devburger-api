39d7d17c0db129e7f8c5065aac84c18d26898afa
luisafalquetoz <luisa_fzandonade@hotmail.com>
2024-07-17 19:14:43 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
+import { password } from './config/database';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-	return response.status(200).json({ message: 'Hello world!' });
+routes.get('/', async (request, response) => {
+	const user = await User.create({
+		id: v4(),
+		name: 'Luisa',
+		email: 'luisafalqueto@email.com',
+		password_hash: 'dygewf87fysdgyfg',
+	});
+
+	return response.status(201).json(user);
 });
 
 export default routes;
